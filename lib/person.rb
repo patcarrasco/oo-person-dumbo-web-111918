dbe8d174a36a6f2539cf0afc6162d78412398062
github username <github email address>
2018-11-21 15:19:36 -0500

    finished

--- lib/person.rb
+++ lib/person.rb
@@ -4,11 +4,12 @@ require 'pry'
 class Person
 
 
-  def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+
+  def initialize(name)
     @name = name
-    @bank_account = bank_account
-    @happiness = happiness
-    @hygiene = hygiene
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
   end
 
   attr_reader :name, :hygiene, :happiness
@@ -18,16 +19,21 @@ class Person
 
 
   def hygiene=(num)
-    if (@hygiene + num) >= 0 && (@hygiene + num) <= 10
-      @hygiene = num
-      # binding.pry
+    @hygiene = num
+    if @hygiene < 0
+      @hygiene = 0
+    elsif @hygiene > 10
+      @hygiene = 10
     end
   end
 
 
   def happiness=(num)
-    if (@happiness + num) >= 0 && (@happiness + num) <= 10
-      @happiness = num
+    @happiness = num
+    if @happiness < 0
+      @happiness = 0
+    elsif @happiness > 10
+      @happiness = 10
     end
   end
 
@@ -46,17 +52,36 @@ class Person
   end
 
   def take_bath
-    hygiene=(4)
-    "♪ Rub-a-dub just relaxing in the tub ♫"
+    self.hygiene=(self.hygiene + 4)
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+    # binding.pry
   end
 
-  # def work_out
-  #   @hygiene -= 3
-  # end
+  def work_out
+    self.hygiene=(self.hygiene - 3)
+    self.happiness=(self.happiness+2)
+    # binding.pry
+    return "♪ another one bites the dust ♫"
+  end
 
-  # def call_friend(person_name)
-  #   person_name.happiness(3)
-  # end
+  def call_friend(person)
+    person.happiness=(person.happiness + 3)
+    self.happiness=(self.happiness + 3)
+    return "Hi #{person.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic.upcase == "POLITICS"
+      self.happiness=(self.happiness - 2)
+      person.happiness=(person.happiness - 2)
+      return "blah blah partisan blah lobbyist"
+    elsif topic.upcase == "WEATHER"
+      self.happiness=(self.happiness + 1)
+      person.happiness=(person.happiness + 1)
+      return "blah blah sun blah rain"
+    else
+      return "blah blah blah blah blah"
+    end
+  end
+
 end
-
-binding.pry
